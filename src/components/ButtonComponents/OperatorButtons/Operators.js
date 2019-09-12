f7517e4562754506f3fbc1e6c8c8eba2d5313c27
amxra <amiradediran@gmail.com>
2019-09-12 10:14:28 +0100

    Add style to operator

--- src/components/ButtonComponents/OperatorButtons/Operators.js
+++ src/components/ButtonComponents/OperatorButtons/Operators.js
@@ -1,6 +1,7 @@
 import React, {useState} from "react";
 import OperatorButton from "./OperatorButton";
 import {operators} from "./../../../data";
+import "./operator.css"
 
 //import any components needed
 
@@ -11,7 +12,7 @@ const Operators = () => {
 
     const [operator, setOperator] = useState(operators)
   return (
-    <div>
+    <div className = {"operatorButton"}>
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
        it any props needed by the child component*/}
