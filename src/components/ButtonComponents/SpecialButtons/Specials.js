677e32f679a1cd6890a499d21d2419195974d055
amxra <amiradediran@gmail.com>
2019-09-12 10:21:52 +0100

    Complete styling

--- src/components/ButtonComponents/SpecialButtons/Specials.js
+++ src/components/ButtonComponents/SpecialButtons/Specials.js
@@ -1,6 +1,7 @@
 import React, {useState} from "react";
 import {specials} from "./../../../data";
 import SpecialButton from "./SpecialButton"
+import "./specials.css"
 
 //import any components needed
 
@@ -12,7 +13,7 @@ const Specials = () => {
   const [special, setSpecial] = useState(specials);
 
   return (
-    <div>
+    <div className = {'specialButton'}>
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
        it any props needed by the child component*/}
